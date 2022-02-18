2fd8850e870d0bf4c767253239f2cf3307b6fb33
zhang <17692347167@163.com>
2022-02-18 12:08:30 +0800

    tag

--- app/src/main/java/com/zhang/mydemo/kotlin/ui/activity/TestTag.kt
+++ app/src/main/java/com/zhang/mydemo/kotlin/ui/activity/TestTag.kt
@@ -1,8 +1,11 @@
 package com.zhang.mydemo.kotlin.ui.activity
 
 import android.widget.Toast
+import com.zhang.mydemo.R
 import com.zhang.mydemo.base.BaseActivity
 import com.zhang.mydemo.kotlin.ui.widgetkt.TagTextUtil
+import com.zhang.mydemo.kotlin.ui.widgetkt.tag.ClickableTextViewMentionLinkOnTouchListener
+import com.zhang.mydemo.kotlin.ui.widgetkt.tag.TimeLineUtility
 import com.zhang.utilslibiary.utils.singleClick
 import kotlinx.android.synthetic.main.layout_title.*
 import kotlinx.android.synthetic.main.text_tag.*
@@ -15,11 +18,13 @@ import kotlinx.android.synthetic.main.text_tag.*
  * @Project Name : MyDemo
  */
 class TestTag : BaseActivity() {
-    override fun getLayoutId(): Int = com.zhang.mydemo.R.layout.text_tag
+    override fun getLayoutId(): Int = R.layout.text_tag
 
     private val contentString = "@王安石 古人之观于天地、^#65&&山川#^、草木、虫鱼、鸟兽，往往有得，以其求思之深而无不在也。" +
             "夫夷以近，则游者众；险以远，则至者少。而世之奇伟、瑰怪，^$1&&非常之观$^，常在于险远，而人之所罕至焉，故非有志者不能至也。有志矣，不随以止也，然力不足者，^#2&&亦不能至也。有志与力，而又不随以怠，至于幽暗昏惑而无物以相之，亦不能至也。#^" +
             "然力足以至焉，于人为可讥，而在己为有悔；尽吾志也而不能至者，可以无悔矣，其孰能讥之乎？^!74&&此余之所得也。!^ --- ^@5232&&王安石@^"
+
+    private val content = "#我的越野世界# #这是个标签#"
 
     override fun initView() {
         tvPageTitle.text = "纯文本标签"
@@ -35,6 +40,11 @@ class TestTag : BaseActivity() {
             ).show()
         }
 
+        tv_tag1.text = TimeLineUtility.convertNormalStringToSpannableString(
+            content,
+            TimeLineUtility.TimeLineStatus.FEED
+        )
+        tv_tag1.setOnTouchListener(ClickableTextViewMentionLinkOnTouchListener())
     }
 
     override fun setListener() {
